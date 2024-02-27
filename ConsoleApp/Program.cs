d90639c69faae564d9bae3f1ac8887a66202ca64
harveytriana <harveytriana@hotmail.com>
2024-02-27 05:06:04 -0500

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿#nullable disable
+// from https://github.com/mattleibow/SkiaSharp-Docker
+// update to net8
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+using SkiaSharp;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine($"Starting the app...");
+
+            var os = RuntimeInformation.OSDescription;
+            Console.WriteLine($"Your operating system is {os}.");
+
+            var assembly = typeof(SKImageInfo).Assembly;
+            var attributes = assembly.GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute));
+            var versionAttr = (AssemblyInformationalVersionAttribute)attributes.FirstOrDefault();
+            Console.WriteLine($"You are using SkiaSharp version {versionAttr?.InformationalVersion}.");
+
+            var colorType = SKImageInfo.PlatformColorType;
+            Console.WriteLine($"The color type for your platform is {colorType}.");
+
+            Console.WriteLine($"All done.");
+        }
+    }
+}
